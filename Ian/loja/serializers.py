33bc6a527049dc7295dca1de97119af71dbc4781
Ian <iansebastiao1234@gmail.com>
2025-03-25 21:48:07 -0300

    Funcionando

--- Ian/loja/serializers.py
+++ Ian/loja/serializers.py
@@ -1,20 +1,64 @@
 from rest_framework import serializers 
 from .models import Cliente, Produto, Venda, ItemDaVenda 
+from django.contrib.auth.models import User
 
 class ClienteSerializer(serializers.ModelSerializer): 
+    username = serializers.CharField(source="user.username", read_only=True)
+
     class Meta: 
         model = Cliente 
         fields = '__all__' 
+
 class ProdutoSerializer(serializers.ModelSerializer): 
     class Meta: 
         model = Produto 
         fields = '__all__' 
+
 class ItemDaVendaSerializer(serializers.ModelSerializer): 
+    product_name = serializers.SerializerMethodField()
+
     class Meta: 
         model = ItemDaVenda 
-        fields = '__all__' 
+        fields = ['id', 'produto', 'quantidade', 'preco_unitario', 'product_name']
+
+    def get_product_name(self, obj): 
+        return obj.produto.nome if obj.produto else None
+    
 class VendaSerializer(serializers.ModelSerializer): 
     itens = ItemDaVendaSerializer(many=True, read_only=True) 
+
     class Meta: 
         model = Venda 
         fields = '__all__'
+    def create(self, validated_data):
+        itens_data = validated_data.pop('itens')
+        venda = Venda.objects.create(**validated_data)
+
+        for item_data in item_data:
+            ItemDaVenda.objects.create(venda=venda, **item_data)
+
+        return venda
+    
+    def to_representation(self, instance):
+        
+        response = super().to_representation(instance)
+        response['itens'] = ItemDaVendaSerializer(instance.itens.all(), many = True)
+        return response
+
+class RegistroSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_onlu=True, min_length=6)
+
+    class Meta:
+        model = Cliente
+        fields = ['username', 'password', 'telefone', 'endereco']
+
+    def create(self, validated_data):
+        username = validated_data.pop('username')
+        password = validated_data.pop('password')
+
+        user = User.objects.create_user(username=username, password=password)
+
+        cliente = Cliente.objects.create(user=user, **validated_data)
+
+        return cliente
--- Ian/loja/views.py
+++ Ian/loja/views.py
@@ -1,7 +1,15 @@
+from rest_framework.permissions import AllowAny
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
 from rest_framework.pagination import PageNumberPagination
 from .models import Cliente, Produto, Venda, ItemDaVenda 
-from .serializers import ClienteSerializer, ProdutoSerializer, VendaSerializer, ItemDaVendaSerializer 
+from .serializers import ClienteSerializer, ProdutoSerializer, RegistroSerializer, VendaSerializer, ItemDaVendaSerializer 
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
 class ClienteViewSet(viewsets.ModelViewSet): 
     queryset = Cliente.objects.all() 
@@ -28,3 +36,51 @@ class ItemDaVendaViewSet(viewsets.ModelViewSet):
     queryset = ItemDaVenda.objects.all() 
     serializer_class = ItemDaVendaSerializer 
     permission_classes = [permissions.IsAuthenticated]
+
+class VendaPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class VendaPorClienteView(APIView):
+    permissions_classes = [IsAuthenticated]
+
+    def get(self, request, cliente_id, format=None):
+        vendas = Venda.objects.filter(cliente_id=cliente_id).order_by('-data_venda')
+
+        paginator = VendaPagination()
+        paginated_vendas = paginator.paginate_queryset(vendas, request)
+        if paginated_vendas is not None:
+            serializer = VendaSerializer(paginated_vendas, many=True)
+            return paginator.get_paginated_response(serializer.data)
+        
+        serializer = VendaSerializer(vendas, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class ClientePorUsernameView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, username, format=None):
+        try:
+            user = User.objects.get(username=username)
+            cliente = Cliente.objects.get(user=user)
+            serializer = ClienteSerializer(cliente)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({'erro': 'Usuário não encontrado'}, status=status.HTTP_404_NOT_FOUND)
+        except Cliente.DoesNotExist:
+            return Response({'erro': 'Cliente não encontrado para este usuário'}, status=status.HTTP_404_NOT_FOUND)
+
+class RegistroView(APIView):
+    permission_classes = [AllowAny]
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+    
+    def post(self, request):
+        serializer = RegistroSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"mensagem": "Cliente cadastrado com sucesso"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
